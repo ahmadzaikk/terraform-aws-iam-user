44e4d60114e013434681398e9b747e71b3d092b1
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2023-02-22 15:50:24 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
-# Managed By : CloudDrove
-# Description : Terraform module to create IAM user resource on AWS.
-# Copyright @ CloudDrove. All Right Reserved.
 
-#Module      : label
-#Description : This terraform module is designed to generate consistent label names and tags
-#              for resources. You can use terraform-labels to implement a strict naming
-#              convention.
-
-
-
-# Module      : IAM user
-# Description : Terraform module to create IAm user resource on AWS.
 resource "aws_iam_user" "default" {
   count = var.enabled ? 1 : 0
 
@@ -20,13 +8,6 @@ resource "aws_iam_user" "default" {
   permissions_boundary = var.permissions_boundary
   tags                 = var.tags
 }
-
-#resource "aws_iam_access_key" "default" {
- # count   = var.enabled ? 1 : 0
-  #user    = aws_iam_user.default.*.name[0]
-  #pgp_key = var.pgp_key
-  #status  = var.status
-#}
 
 resource "aws_iam_user_policy" "default" {
   count  = var.enabled && var.policy_enabled && var.policy_arn != "" ? 1 : 0
@@ -50,16 +31,4 @@ resource "aws_iam_user_group_membership" "default" {
   depends_on = [aws_iam_user.default]
 }
 
-#resource "aws_iam_user_login_profile" "default" {
- # count                   = var.create_user && var.create_iam_user_login_profile ? 1 : 0
-  #user                    = aws_iam_user.default[0].name
-  #password_length         = var.password_length
-  #password_reset_required = var.password_reset_required
-#}
 
-#resource "aws_iam_user_ssh_key" "default" {
- # count      = var.create_user && var.upload_iam_user_ssh_key ? 1 : 0
- # username   = aws_iam_user.default[0].name
-  #encoding   = var.ssh_key_encoding
- # public_key = var.ssh_public_key
-#}
