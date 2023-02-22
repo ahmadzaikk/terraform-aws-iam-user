9d1f2ce3600055bedf324cd7e7fc0d11c6000d5b
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2023-02-22 13:40:04 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,14 +10,4 @@ output "unique_id" {
   description = "The unique ID assigned by AWS for this user."
 }
 
-output "key_id" {
-  value       = join("", aws_iam_access_key.default.*.id)
-  description = "The access key ID."
-}
-
-output "secret" {
-  value       = var.pgp_key == "" ? join("", aws_iam_access_key.default.*.secret) : join("", aws_iam_access_key.default.*.encrypted_secret)
-  description = "The secret access key. Note that this will be written to the state file. Please supply a pgp_key instead, which will prevent the secret from being stored in plain text."
-  sensitive   = true
-}
 
